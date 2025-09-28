4f499eaf69b31a17f9aa0ea151507b79c96d9c59
Rayann <mr3286124@gmail.com>
2025-09-28 14:59:35 +0500

    Loading Added

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -21,7 +21,7 @@ export default function AirdropForm(){
     const {data: hash,isPending,writeContractAsync} = useWriteContract()
     const [tokenName, setTokenName] = useState<string>("-")
     const [tokenDecimals, setTokenDecimals] = useState<number>(18)
-
+    const [isLoading, setIsLoading] = useState(false);
 
 
 useEffect(() => {
@@ -107,6 +107,7 @@ const totalTokens = totalWei / Math.pow(10, tokenDecimals)
 
     async function handleSubmit(){
 
+             setIsLoading(true);
         const tsenderAddress = chainsToTSender[chainId]["tsender"]
         const approvedAmount = await getApprovedAmount(tsenderAddress)
 
@@ -153,6 +154,8 @@ const totalTokens = totalWei / Math.pow(10, tokenDecimals)
                 ],
             },)
         }
+            setIsLoading(false);
+
 
     }
 
@@ -213,12 +216,28 @@ const totalTokens = totalWei / Math.pow(10, tokenDecimals)
 
             <br />
 
-        <button 
+<button 
   onClick={handleSubmit}
-  className="w-half bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg transition-all duration-200 ease-in-out hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+  disabled={isLoading}
+  className={`w-half bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg transition-all duration-200 ease-in-out hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 ${
+    isLoading ? 'opacity-50 cursor-not-allowed' : ''
+  }`}
 >
-  Send Tokens
+  {isLoading ? (
+    <div className="flex items-center justify-center">
+      <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+      Confirming in Wallet
+    </div>
+  ) : (
+    "Send Tokens"
+  )}
 </button>
+
+
+
         </div>
     )
 
